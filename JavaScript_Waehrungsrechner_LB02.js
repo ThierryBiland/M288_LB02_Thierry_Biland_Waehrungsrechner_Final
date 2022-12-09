298857e59c43a65232b77d50e2d1baa1a8097414
thierryswisscom <thierrybiland06@gmail.com>
2022-12-09 12:49:00 +0100

    1

--- JavaScript_Waehrungsrechner_LB02.js
+++ JavaScript_Waehrungsrechner_LB02.js
@@ -1,1 +1,7 @@
-const
+const währung = document.querySelectorAll(".währung")
+const zahlenbox = document.getElementById("zahlenbox")
+const ergebnisbox = document.getElementById("ergebnisbox")
+const button = document.getElementById("button")
+
+fetch("https://api.frankfurter.app/currencies")
+    .then(data) => data.json)
