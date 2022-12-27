27788c4245b8350a5c583e88aaf1a23d9c41d194
thierryswisscom <thierrybiland06@gmail.com>
2022-12-27 07:50:40 +0100

    1

--- JavaScript_Waehrungsrechner_LB02.js
+++ JavaScript_Waehrungsrechner_LB02.js
@@ -1,4 +1,4 @@
-const währung = document.querySelectorAll(".währung")
+const waehrung = document.querySelectorAll(".währung")
 const zahlenbox = document.getElementById("zahlenbox")
 const ergebnisbox = document.getElementById("ergebnisbox")
 const button = document.getElementById("button")
@@ -12,14 +12,14 @@ fetch("https://api.frankfurter.app/currencies")
 function display(data) {
     const entries = Object.entries(data);
     for (var i = 0; i < entries.length; i++) {
-        währung[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-        währung[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+        waehrung[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+        waehrung[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
     }
 }
 
 button.addEventListener("click"  , () => {
-    let currency1 = währung[0].value;
-    let currency2 = währung[1].value;
+    let currency1 = waehrung[0].value;
+    let currency2 = waehrung[1].value;
     let value = zahlenbox.value;
 
     if (currency1 != currency2) {
