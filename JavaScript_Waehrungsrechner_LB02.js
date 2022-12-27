af488b2803a5c0b861a0051927e44f52caf39fd7
thierryswisscom <thierrybiland06@gmail.com>
2022-12-27 07:41:17 +0100

    1

--- JavaScript_Waehrungsrechner_LB02.js
+++ JavaScript_Waehrungsrechner_LB02.js
@@ -11,7 +11,7 @@ fetch("https://api.frankfurter.app/currencies")
 
 function display(data) {
     const entries = Object.entries(data);
-    for (var i = 0; i < entries.lenght; i++) {
+    for (var i = 0; i < entries.length; i++) {
         währung[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
         währung[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
     }
